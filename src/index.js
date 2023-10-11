241e0f9386de5cfb2628814f64c2f23e7e12b0cb
azimjon-95 <azimjon995m@gmail.com>
2023-10-11 21:11:52 +0500

    metor

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import heck from './assets/hecker.jpg'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  < React.StrictMode >
+    {/* <App /> */}
+    <div className='heck'>
+      <h1>Hello World!</h1>
+      <img src={heck} alt="" />
+    </ div>
+  </React.StrictMode >
 );
 
